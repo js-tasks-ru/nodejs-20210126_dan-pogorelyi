8d994191d95839b6e0efb5a9936fe18d90f1cdde
Pogorelyi Daniil <daniil.pogorelyi@nordigy.ru>
2021-02-23 19:12:06 +0300

    add solution for 06-koajs 01-chat-app

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -1,17 +1,31 @@
-const path = require('path');
-const Koa = require('koa');
+const path = require("path");
+const Koa = require("koa");
 const app = new Koa();
+const EventEmitter = require("events");
 
-app.use(require('koa-static')(path.join(__dirname, 'public')));
-app.use(require('koa-bodyparser')());
+app.use(require("koa-static")(path.join(__dirname, "public")));
+app.use(require("koa-bodyparser")());
 
-const Router = require('koa-router');
+const Router = require("koa-router");
 const router = new Router();
+const messageEmitter = new EventEmitter();
 
-router.get('/subscribe', async (ctx, next) => {
+router.get("/subscribe", async (ctx) => {
+  const message = await new Promise((resolve) => {
+    messageEmitter.once("message", (message) => resolve(message));
+  });
+
+  ctx.body = message;
 });
 
-router.post('/publish', async (ctx, next) => {
+router.post("/publish", (ctx) => {
+  const message = ctx.request.body.message;
+
+  if (message) {
+    messageEmitter.emit("message", message);
+  }
+
+  ctx.body = "It's all right";
 });
 
 app.use(router.routes());
