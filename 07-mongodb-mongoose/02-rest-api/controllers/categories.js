3e9af1cac397452f10677ec1a3e37882674ed09b
Pogorelyi Daniil <daniil.pogorelyi@nordigy.ru>
2021-03-03 00:37:28 +0300

    add solution for 07-mongodb-mongoose 02-rest-api

--- 07-mongodb-mongoose/02-rest-api/controllers/categories.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/categories.js
@@ -1,3 +1,20 @@
-module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+const Categories = require("../models/Category");
+
+const getFormattedCategories = (categories) =>
+  categories.map((category) => ({
+    id: category.id,
+    title: category.title,
+    subcategories: getFormattedSubCategories(category.subcategories),
+  }));
+
+const getFormattedSubCategories = (subCategories) =>
+  subCategories.map((subCategory) => ({
+    id: subCategory.id,
+    title: subCategory.title,
+  }));
+
+module.exports.categoryList = async function categoryList(ctx) {
+  const categories = await Categories.find();
+
+  ctx.body = { categories: getFormattedCategories(categories) };
 };
